0550dda25e69910e65ed541b2c8b65a2c0a60768
Abid Khan <abidkhan0786@gmail.com>
2022-09-30 15:40:49 +0530

    Committed Calculation.py file

---
+++ Calculation.py
@@ -0,0 +1,12 @@
+class Calculation:
+    a=0
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+
+    def addition(self):
+        self.num1+self.num2
+print('Hello',6)
+    
+c=Calculation(2,4)
+c.addition()
